4e33f8b742b9caac3da835275a03e832ae88c0de
BeliaevAndrey <ab5030107021@yandex.ru>
2026-02-19 01:07:03 +0300

    config.py added

---
+++ config.py
@@ -0,0 +1,12 @@
+"""
+Конфигурация из переменных окружения.
+"""
+
+import os
+
+API_ID = int(os.environ.get("API_ID", "0"))
+API_HASH = os.environ.get("API_HASH", "")
+PHONE = os.environ.get("PHONE", "")
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "")
+OPENAI_MODEL = os.environ.get("OPENAI_MODEL", "gpt-4o-mini")
